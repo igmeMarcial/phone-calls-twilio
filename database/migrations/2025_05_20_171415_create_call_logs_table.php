aadff7d9dd909d35b51cf56c26dfdf3c3f1ef106
Marcial Igme <igmemarcial@gmail.com>
2025-05-22 12:17:47 -0500

    feat: add migrations for  call_logs tables

--- database/migrations/2025_05_20_171415_create_call_logs_table.php
+++ database/migrations/2025_05_20_171415_create_call_logs_table.php
@@ -12,17 +12,17 @@ return new class extends Migration {
     {
         Schema::create('call_logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Quién hizo la llamada
-            $table->string('destination_number'); // Número al que se llamó
-            $table->string('twilio_call_sid')->unique()->nullable(); // SID de la llamada en Twilio
-            $table->string('status')->default('initiated'); // Estado (queued, ringing, answered, completed, failed, etc.)
-            $table->timestamp('start_time')->nullable(); // Hora de inicio real (según callback de Twilio)
-            $table->timestamp('end_time')->nullable();   // Hora de fin (según callback de Twilio)
-            $table->integer('duration')->nullable(); // Duración en segundos (según callback)
-            $table->decimal('price', 8, 5)->nullable(); // Costo (según callback)
-            $table->text('error_message')->nullable(); // Mensaje de error si falla
-            $table->timestamps(); // created_at (cuando se inició en el backend), updated_at
-            $table->foreignId('phone_number_id')->nullable()->constrained()->onDelete('cascade'); // Número desde el que se llamó
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('destination_number');
+            $table->string('twilio_call_sid')->unique()->nullable();
+            $table->string('status')->default('initiated');
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->integer('duration')->nullable();
+            $table->decimal('price', 8, 5)->nullable();
+            $table->text('error_message')->nullable();
+            $table->timestamps();
+            $table->foreignId('phone_number_id')->nullable()->constrained()->onDelete('cascade');
         });
     }
 
